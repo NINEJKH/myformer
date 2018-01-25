3675c94384b8570e98d80f79d599173e467b03c3
Gunter Grodotzki <gunter@grodotzki.co.za>
2018-01-25 17:12:26 +0200

    fix large strings (#7)

--- cli.php
+++ cli.php
@@ -11,6 +11,13 @@ ini_set('xdebug.var_display_max_children', -1);
 ini_set('xdebug.var_display_max_data', -1);
 ini_set('xdebug.var_display_max_depth', -1);
 
+// this is needed to regex larger lines
+if (!empty(getenv('MYFORMER_PCRE_BACKTRACK_LIMIT'))) {
+    ini_set('pcre.backtrack_limit', getenv('MYFORMER_PCRE_BACKTRACK_LIMIT'));
+} else {
+    ini_set('pcre.backtrack_limit', 1024 * 1024 * 10);
+}
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Commands\TransformCommand;
