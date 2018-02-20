b71e11cc9cee6ca8a125b9a1d56f87c4c26a5bdc
Gunter Grodotzki <gunter@grodotzki.co.za>
2018-02-20 15:37:56 +0200

    make tel prefixable

--- app/Transformer/Transformers/Tel.php
+++ app/Transformer/Transformers/Tel.php
@@ -4,8 +4,22 @@ namespace App\Transformer\Transformers;
 
 class Tel extends Transformer
 {
+    protected $prefix = '';
+
     public function transform(array $columns_n, array $values, $column)
     {
-        return $this->replaceValue($values[$column], sprintf('999%d', rand(1000000000, 9999999999)));
+        if (!empty($this->param)) {
+            $prefix = 0;
+        }
+        return $this->replaceValue($values[$column], sprintf('%s999%d', $this->prefix, rand(1000000000, 9999999999)));
+    }
+
+    public function setParam($param)
+    {
+        $this->param = $param;
+
+        if (!empty($param)) {
+            $this->prefix = $param;
+        }
     }
 }
